68f68e8c526f73c3fcb28ea7911dc13beba384cf
seo <c0a230874c@edu.teu.ac.jp>
2024-09-24 16:56:56 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -17,18 +17,20 @@ def main():
     img_rct.center = 300, 200 #練習8
     tmr = 0
     while True:
+        ud = 0 #演習2
+        rl = -1 #演習2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        img_rct.move_ip((-1,0)) #演習1
         key_list = pg.key.get_pressed() #練習8
         if key_list[pg.K_UP]: #上移動
-            img_rct.move_ip((0, -1)) 
+            ud +=-1 #演習2
         if key_list[pg.K_DOWN]: #下移動
-            img_rct.move_ip((0, 1)) 
+            ud += 1 #演習2
         if key_list[pg.K_RIGHT]: #右移動
-            img_rct.move_ip((2, 0)) #演習1
+            rl += 2 #演習2
         if key_list[pg.K_LEFT]: #左移動
-            img_rct.move_ip((-1,0)) 
+            rl += -1 #演習2
+        img_rct.move_ip((rl,ud)) #演習2
         x = -(tmr%3200) #練習7
         screen.blit(bg_img, [x, 0]) #練習6
         screen.blit(bg_flip, [x+1600, 0]) #練習7
